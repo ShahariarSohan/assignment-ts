92db16b5f31e9da3e43f39a432ce7835e16d85a1
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-05-09 18:04:50 +0600

    some problem solution and README.md file added

---
+++ README.md
@@ -0,0 +1,51 @@
+## Blogs
+
+### Enums
+
+In a simple way enums define a set of named constants.In typescript we use enums to work with set of values in a typical manner that means type safe manner. Because in Typescript static type is the main concept.So enums has create a convenient way to improve code readability and maintainability with set of values.
+
+#### Enums can be either numeric or string-based. Here is the examples :
+
+#### String Enum
+
+- String enums require explicit string values for each member
+
+#### Examples :
+
+enum Color {
+Red = "RED",
+Green = "GREEN",
+Blue = "BLUE"
+Black="Black"
+}
+let myColor: Color = Color.Green;
+console.log(myColor); // Output: "GREEN"
+
+#### Numeric Enum
+
+- Numeric enums auto-increment numbers (but you can set starting values)
+
+#### Examples :
+
+enum Direction {
+Up, // 0
+Down, // 1
+Left, // 2
+Right // 3
+}
+let move: Direction = Direction.Up;
+console.log(move); // Output: 0
+
+### Type Inference
+
+Type inference is a built-in feature of typescript. It is a feature that helps compiler to determine type of variable without explicit type .That means when the type of a variable is implicit .The inference feature auto determine the of variable.Which makes the life easy for developers of this generation.This is one of reason that typescript becoming famous day by day.
+
+#### Examples
+
+const name="Jhankar Mahbub";
+const number =10;
+const isTrue =true; // here typescript can inferred type easily but for some cases complex typescript will able to infer type.
+const user={
+name:"Mir Hossain"
+contactNo:"Nai"
+}
--- src/Problems.ts
+++ src/Problems.ts
@@ -1,1 +1,101 @@
-console.log("Hello");
+function formatString(input: string, booleanValue?: boolean): string {
+  if (booleanValue === false) {
+    return input.toLowerCase();
+  } else {
+    return input.toUpperCase();
+  }
+}
+
+//
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  const result = items.filter((item) => item.rating >= 4);
+  return result;
+}
+
+//
+
+//
+class Vehicle {
+  private make: string;
+  private year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+  getInfo() {
+    console.log(`Make : ${this.make} , Year: ${this.year}`);
+  }
+}
+class Car extends Vehicle {
+  private model: string;
+
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
+  getModel() {
+    console.log(`Model : ${this.model}`);
+  }
+}
+//
+function processValue(value: string | number): number {
+  if (typeof value === "string") {
+    return value.length;
+  } else {
+    return value * 2;
+  }
+}
+//
+interface Product {
+  name: string;
+  price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+  if (products.length === 0) {
+    return null;
+  } else {
+    const result1 = products?.sort(
+      (product1, product2) => product2.price - product1.price
+    );
+    const result2 = result1?.find((product) => product);
+    return result2;
+  }
+}
+const products = [
+  { name: "Pen", price: 100 },
+  { name: "Notebook", price: 2500 },
+  { name: "Bag", price: 5 },
+  { name: "Bag", price: 400 },
+  { name: "Bag", price: 5 },
+];
+console.log(getMostExpensiveProduct(products));
+// Output: { name: "Bag", price: 50 })
+//
+async function squareAsync(value: number): Promise<number> {
+  return new Promise((resolve, reject) => {
+    if (value >= 0) {
+      setTimeout(() => {
+        resolve(value * value);
+      }, 1000);
+    } else {
+      reject("Negative number not allowed");
+    }
+  });
+}
+enum Day {
+  Monday = "Weekday",
+  Tuesday = "Weekday",
+  Wednesday = "Weekday",
+  Thursday = "Weekday",
+  Friday = "Weekday",
+  Saturday = "Weekday",
+  Sunday = "Weekend",
+}
+
+function getDayType(day: Day): string {
+  return day;
+}
